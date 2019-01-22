08bc388cc315ef20d4085e696f36bfbb914074e5
Herman <hrivas5726@aol.com>
2019-01-22 03:54:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,20 @@
 # your code goes here
 array = ["ruby", "rspec", "sails"]
 
-def begind_with_r(array) 
- 
- if array.all? {|word| word.start_with?("r")}
- else
+def begind_with_r(arr) 
+    arr.all? do |string| 
+    string[0] == "r"
 end
 
 def contain_a(array)
 	array.find_all {|word| word.include?("a")}
+end
+
+def remove_non_strings(array)
+	array.delete_if {|string| string.class != String}
+end
+
+def count_elements(array)
+ 
 end
   
