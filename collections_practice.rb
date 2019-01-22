b465a31660753d2836719679f0adb98118abcdf1
Herman <hrivas5726@aol.com>
2019-01-22 03:35:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
 # your code goes here
+array = ["ruby", "rspec", "sails"]
+
+def begind_with_r(array) 
+ 
+ if array.all? {|word| word.start_with?("r")}
+ else
+end
+
+def contain_a(array)
+	array.find_all {|word| word.include?("a")}
+end
+  
